eb40bca713ae58658b31f75429e523dc8f5595ac
FREDERIC MAQUET <frederic.maquet@ext.adeo.com>
2017-07-24 12:53:05 +0200

    compil et run en java 8

---
+++ compile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -rf classes/main
+rm -rf classes/outils
+
+javac -sourcepath src src/main/*.java -d classes
+
+# ATTENTION il faut bien le sourcepath si tu fais : javac src/main/*.java -d classes ==> KO
+
