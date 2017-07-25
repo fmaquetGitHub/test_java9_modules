3bc8d8d73586d918495c502776ad7bb61c09dc78
FREDERIC MAQUET <frederic.maquet@ext.adeo.com>
2017-07-25 11:54:53 +0200

    modification compile_j9.sh avec plusieurs lignes pour la compilation

--- compile_j9.sh
+++ compile_j9.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-rm -rf classes/*
-rm -rf modules/*
+rm -rf classes/
+rm -rf modules/
 
 #javac --module-source-path src $(find src -name "*.java") -d mods
 
-javac --source-path src $(find src -name "*.java") -d modules
 
+# OK
+#javac --source-path src $(find src -name "*.java") -d modules
+
+# OK
+javac src/com.norsys/module-info.java src/com.norsys/norsys/main/Main.java -d modules
 
 
