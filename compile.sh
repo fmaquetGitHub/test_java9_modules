0d75fd6f08b0e69af7051d3ea3b0b3a346d6c581
FREDERIC MAQUET <frederic.maquet@ext.adeo.com>
2017-07-27 10:31:21 +0200

    [GREETINGS] : Deux modules

--- compile.sh
+++ compile.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+#http://openjdk.java.net/projects/jigsaw/quick-start
 
-rm -rf modules
+rm -rf mod*
 
-javac -d modules/com.greetings \
-        src/com.greetings/module-info.java \
-        src/com.greetings/com/greetings/Main.java
-        
+#### compilation du module org.astro
+#javac -d modules/org.astro src/org.astro/module-info.java src/org.astro/org/astro/World.java
 
-        
-        
+# compilation du module org.astro (en plus court)
+javac -d modules/org.astro $(find src/org.astro -name "*.java")
+
+#### compilation du module com.greetings
+#javac --module-path modules -d modules/com.greetings src/com.greetings/module-info.java \
+#					   src/com.greetings/com/greetings/Main.java
+
+
+# TOUT COMPILER EN UNE FOIS
+javac -d modules --module-source-path src $(find src -name "*.java")
+
+
+#javac -d modules --module-source-path src $(find src -name "*.java")
